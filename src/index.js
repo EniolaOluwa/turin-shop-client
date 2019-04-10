3409acea2a452edd55935664cc49efe0637dec40
enirate <enirate@gmail.com>
2019-04-10 22:24:41 +0100

    Initial commit: Setup react boilerplate from scratch

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const title = 'Hello World!';
+
+render(<div>{title}</div>, document.getElementById('app'));
+
+module.hot.accept();
